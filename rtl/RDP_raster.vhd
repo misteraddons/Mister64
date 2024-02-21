0fb36d16dd4354a788060cc6a21a99474add14ad
RobertPeip <->
2024-02-21 15:43:35 +0100

    RDP: implement interlace scissor (e.g. Factor5 games title screens)

--- rtl/RDP_raster.vhd
+++ rtl/RDP_raster.vhd
@@ -146,6 +146,7 @@ architecture arch of RDP_raster is
    signal unscrx        : signed(12 downto 0) := (others => '0');
    signal maxxmx        : unsigned(11 downto 0) := (others => '0');
    signal minxmx        : unsigned(11 downto 0) := (others => '0');
+   signal scissorNow    : std_logic;
       
    type t_majorminor is array(0 to 3) of unsigned(12 downto 0);
    signal majorx        : t_majorminor := (others => (others => '0'));
@@ -499,6 +500,10 @@ begin
                      '1' when (ycur >= yllimit) else
                      '1' when (loading_mode = '0' and curcross = '1') else
                      '0';
+                     
+   scissorNow     <= '0' when (settings_scissor.ScissorField = '0') else
+                     '0' when (settings_scissor.ScissorOdd /= ycur(2)) else
+                     '1';
                    
    calcFBAddr     <= resize(((lineInfo.y * (settings_colorImage.FB_width_m1 + 1)) + lineInfo.xStart) * 1, 26) when (settings_colorImage.FB_size = SIZE_8BIT) else
                      resize(((lineInfo.y * (settings_colorImage.FB_width_m1 + 1)) + lineInfo.xStart) * 2, 26) when (settings_colorImage.FB_size = SIZE_16BIT) else
@@ -714,7 +719,7 @@ begin
                   end if;
                   
                when REQUESTFB =>
-                  if (allinval = '0' and allover = '0' and allunder = '0') then
+                  if (allinval = '0' and allover = '0' and allunder = '0' and scissorNow = '0') then
                      polystate <= WAITREADRAM;
                      FBreq     <= '1';
                      FBaddr    <= calcFBAddr;
@@ -755,7 +760,7 @@ begin
                startLine <= '0';
             end if;
             
-            if (startLine = '1' and linestate = LINEIDLE and (allinval = '1' or allover = '1' or allunder = '1')) then
+            if (startLine = '1' and linestate = LINEIDLE and (allinval = '1' or allover = '1' or allunder = '1' or scissorNow = '1')) then
                startLine <= '0';
             end if;
             
@@ -894,7 +899,7 @@ begin
                      end if;
                   end if;
                
-                  if (startLine = '1' and allinval = '0' and allover = '0' and allunder = '0') then
+                  if (startLine = '1' and allinval = '0' and allover = '0' and allunder = '0' and scissorNow = '0') then
                      if (loading_mode = '0') then
                         if (settings_otherModes.cycleType = "11") then
                            linestate <= FILLLINE;
